fd8fe45a1abfe0e6e150e5ed61891a2ee445d143
mubesy <mahussain@scu.edu>
2021-08-03 13:35:05 -0700

    updated todoapp changes

---
+++ todoapp/urls.py
@@ -0,0 +1,37 @@
+"""todoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path
+from rest_framework import routers
+from todolist import views
+from django.conf.urls import url
+from django.contrib import admin
+from todolist.views import index
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('ping', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('todo', include(router.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="TodoList"),
+]
+
---
+++ todolist/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from .models import TodoList, Category
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from todolist.serializers import UserSerializer, GroupSerializer
+
+
+def index(request): #the index view
+    todos = TodoList.objects.all() #quering all todos with the object manager
+    categories = Category.objects.all() #getting all categories with object manager
+    if request.method == "POST": #checking if the request method is a POST
+        if "taskAdd" in request.POST: #checking if there is a request to add a todo
+            title = request.POST["description"] #title
+            date = str(request.POST["date"]) #date
+            category = request.POST["category_select"] #category
+            content = title + " -- " + date + " " + category #content
+            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+            Todo.save() #saving the todo 
+            return redirect("/") #reloading the page
+        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+            for todo_id in checkedlist:
+                todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+                todo.delete() #deleting todo
+    return render(request, "index.html", {"todos": todos, "categories":categories})
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
